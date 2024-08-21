d8d7f7de59cabbdc5c464ace04fea1ebf8a21bab
Tariq Ali <tarikali7444@gmail.com>
2024-08-21 23:56:20 +0530

    is_active made true

--- core/behaviours.py
+++ core/behaviours.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class StatusMixin(models.Model):
     uid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-    isActive = models.BooleanField(default=False,blank=True)
+    isActive = models.BooleanField(default=True,blank=True)
     created_at= models.DateTimeField(auto_now_add=True,blank=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True)
     class Meta:
