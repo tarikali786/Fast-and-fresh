80198de51d41baac6a1f3d889c544146a0049c68
Tariq Ali <tarikali7444@gmail.com>
2024-09-06 15:43:46 +0530

    update status

--- college/views.py
+++ college/views.py
@@ -949,7 +949,7 @@ class CollectionTaskviewset(viewsets.GenericViewSet):
             collection_instances = Collection.objects.filter(Q(current_status="DELIVERED_TO_WAREHOUSE") | Q(current_status="WASHING") )
         
         elif employee_instance.employee_type == "Drying":
-            collection_instances = Collection.objects.filter(Q(current_status="WASHING_DONE") | Q(current_status="Drying"))
+            collection_instances = Collection.objects.filter(Q(current_status="WASHING_DONE") | Q(current_status="DRYING"))
         
         elif employee_instance.employee_type == "Segregation":
             collection_instances = Collection.objects.filter(Q(current_status="DRYING_DONE") |Q(current_status="IN_SEGREGATION"))
